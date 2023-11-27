d31cbb52436614a8aa30e029acbbeb6a7e8fc424
Tommaso Castagnoli <tommasocastagnoli29@gmail.com>
2023-11-27 15:28:14 +0100

    Aggiungere i file di progetto.

---
+++ 24.1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1
+{
+    public class Quadrato
+    {
+        public int MyProperty { get; set; }
+        public float Lato;
+
+       
+    }
+}
---
+++ 24.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    
+            
+            Quadrato q= new Quadrato();
+
+            Quadrato q1 = q;
+
+            
+            Console.WriteLine("Inserisci il lato");
+            q1.Lato= float.Parse(Console.ReadLine());
+
+            float superifice;
+
+            Console.WriteLine("il quadrato ha superfice pari a {0} ",q1.Lato*q1.Lato);
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
